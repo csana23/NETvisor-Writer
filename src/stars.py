f7917ea4c87a73956da0b31cb7d1c74cc6324bc3
Csanaki Richárd <richard.csanaki@gmail.com>
2019-05-05 16:12:58 +0200

    Create stars.py

---
+++ src/stars.py
@@ -0,0 +1,24 @@
+n = 1
+s = 2
+
+while s >= 0:
+    print(' ' * s, end='')
+    print('* ' * n)
+    print('', end='')
+
+    s = s-1
+    n = n+1
+
+print('')
+
+n = 3
+s = 0
+
+while s <= 2:
+    print(' ' * s, end='')
+    print('* ' * n)
+    print('', end='')
+
+    s = s+1
+    n = n-1
+
