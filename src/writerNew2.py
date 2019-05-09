5f5e132fa92fd778947f8da45920764d00ecb041
Csanaki Richárd <richard.csanaki@gmail.com>
2019-05-09 13:27:07 +0200

    update

--- src/writerNew2.py
+++ src/writerNew2.py
@@ -3,6 +3,19 @@ import paho.mqtt.client as client
 import paho.mqtt.publish as publish
 import sys
 import json
+import os
+
+if os.path.exists('writer.log'):
+    os.remove('writer.log')
+
+if os.path.exists('writer-err.log'):
+    os.remove('writer-err.log')
+
+log = open('writer.log', 'a+')
+log_err = open('writer-err.log', 'a+')
+
+sys.stdout = log
+sys.stderr = log_err
 
 params = []
 
@@ -19,11 +32,11 @@ for i in range(1, 6):
 paramDict = {}
 myclient = client.Client(client_id="ricsi_yolo", clean_session=True, userdata=None, transport="tcp")
 
-class WriterNew:
-
-    for i in range(len(params)):
+for i in range(len(params)):
         content_of_element = json.loads(params[i])
         paramDict.update(content_of_element)
+
+class WriterNew:
 
     def on_connect(self, client, userdata, flags, rc):
         print("CONNACK received with code ", rc)
@@ -32,7 +45,7 @@ class WriterNew:
             print("Connected OK ", rc)
 
             publish.single(str(paramDict['topicAlive']), payload='{"module":"writer"}', qos=2, hostname=paramDict['vezerles']['ip'])
-            print("alive sent...")
+           # print("alive sent...")
 
         else:
             print("Connection failed, Returned code: ", rc)
@@ -50,15 +63,17 @@ class WriterNew:
         payload = parsed['data']
 
         # send to shub
-        publish.single(paramDict['sensorhub']['topic_to_send'], payload=payload, qos=2, hostname=paramDict['sensorhub']['ip'])
+        publish.single(paramDict['sensorhub']['topic_to_send'],
+                       payload=payload, qos=2, hostname=paramDict['sensorhub']['ip'])
 
         # send ack to vezerles
         id = parsed['id']
         msg_ack = '{"id":' + '"' + str(id) + '"}'
 
-        result = myclient.publish(paramDict['topicToPublish'], payload=msg_ack, qos=2)
-        print("result of ack: " + result)
-    
+        result = myclient.publish(
+            paramDict['topicToPublish'], payload=msg_ack, qos=2)
+        print("result of sending: " + result)
+
     def parse_message(self, incomingJson):
         parsed = {}
         content_of_element = json.loads(incomingJson)
@@ -71,12 +86,14 @@ class WriterNew:
 
         return parsed
 
+
 w = WriterNew()
 
 myclient.on_connect = w.on_connect
 myclient.on_message = w.on_message
 
 myclient.connect(paramDict['vezerles']['ip'], int(paramDict['vezerles']['port']))
+print('alive sent...')
 
 topic_to_subscribe = str(paramDict['topicToSubscribe'])
 print("topic_to_subscribe:", topic_to_subscribe)
